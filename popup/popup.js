557e5e585efbb27af0f72552ac2207cf470ce705
Zheng <zhengzheng@brandeis.edu>
2024-02-13 14:04:47 -0500

    record history and update show page when history is updating(finished)

--- popup/popup.js
+++ popup/popup.js
@@ -1,107 +1,157 @@
-
-
 function logTabsForWindows(windowInfoArray) {
-  const window_tamplate = document.getElementById('window_template');
+  document.querySelector(".windows").innerHTML = "";
+  const window_tamplate = document.getElementById("window_template");
   const tab_template = document.getElementById("tab_template");
   for (const windowInfo of windowInfoArray) {
     const win_elem = window_tamplate.content.firstElementChild.cloneNode(true);
-    win_elem.querySelector('.window-title').textContent = windowInfo.tabs.length + " tabs";
+    win_elem.querySelector(".window-title").textContent =
+      windowInfo.tabs.length + " tabs";
     for (const tab of windowInfo.tabs) {
       const element = tab_template.content.firstElementChild.cloneNode(true);
       if (tab.title.length > 45) {
-        element.querySelector('.tab-title').textContent = tab.title.substring(0, 45) + "...";
+        element.querySelector(".tab-title").textContent =
+          tab.title.substring(0, 45) + "...";
       } else {
-        element.querySelector('.tab-title').textContent = tab.title;
+        element.querySelector(".tab-title").textContent = tab.title;
       }
       element.querySelector(".tab-icon").src = tab.favIconUrl;
       win_elem.querySelector(".window-tabs").append(element);
     }
-    document.querySelector(".windows").append(win_elem); 
+    document.querySelector(".windows").append(win_elem);
   }
 }
 
-function logTabsFormHistory (historyArray) {
-  const window_tamplate = document.getElementById('window_template');
+function logTabsFormHistory(historyArray) {
+  document.querySelector(".windows").innerHTML = "";
+  const window_tamplate = document.getElementById("window_template");
   const tab_template = document.getElementById("tab_template");
-
   for (const window of historyArray.reverse()) {
     const win_elem = window_tamplate.content.firstElementChild.cloneNode(true);
-      win_elem.querySelector('.window-title').textContent = window.length + " tabs";
-      for (const tab of window) {
-        const element = tab_template.content.firstElementChild.cloneNode(true);
-        console.log(element);
-        if (tab.title.length > 45) {
-          element.querySelector('.tab-title').textContent = tab.title.substring(0, 45) + "...";
-        } else {
-          element.querySelector('.tab-title').textContent = tab.title;
-        }
-        element.querySelector(".tab-icon").src = tab.favIconUrl;
-        win_elem.querySelector(".window-tabs").append(element);
+    win_elem.querySelector(".window-title").textContent =
+      window.length + " tabs";
+    for (const tab of window) {
+      const element = tab_template.content.firstElementChild.cloneNode(true);
+      if (tab.title.length > 45) {
+        element.querySelector(".tab-title").textContent =
+          tab.title.substring(0, 45) + "...";
+      } else {
+        element.querySelector(".tab-title").textContent = tab.title;
       }
-      document.querySelector(".windows").append(win_elem); 
+      element.querySelector(".tab-icon").src = tab.favIconUrl;
+      win_elem.querySelector(".window-tabs").append(element);
+    }
+    document.querySelector(".windows").append(win_elem);
   }
 }
 
+async function readFromStorage(key) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], (result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result[key] || null);
+      }
+    });
+  });
+}
+
+async function writeToStorage(key, data) {
+  return new Promise((resolve, reject) => {
+    const dataToStore = {};
+    dataToStore[key] = data;
+    chrome.storage.local.set(dataToStore, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
 function onError(error) {
   console.error(`Error: ${error}`);
 }
 
 // Initialization
-var is_restore_mode = false;
-chrome.storage.local.get("mode", function(data) {
-  if (typeof data.mode === "undefined") {
-    chrome.storage.local.set({mode : false}, function() {
-      if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-          return;
-      }
-    });
-  } else {
-    is_restore_mode = data.mode;
+const clear = document.querySelector(".clear-button");
+const toggle = document.querySelector(".restore-toggle-button");
+
+let is_restore_mode = await readFromStorage("mode");
+if (is_restore_mode == null) {
+  writeToStorage("mode", false);
+}
+if (is_restore_mode) {
+  let history = await readFromStorage("history");
+  if (history == null) {
+    history = [];
   }
-  if (is_restore_mode) {
-    chrome.storage.local.get({history : []}, function(data) {
-      logTabsFormHistory(data.history);
-    })
-    toggle.classList.toggle("active");
-  } else {
-    chrome.windows.getAll({populate: true})
-      .then(logTabsForWindows, onError);
-  }
-})
-
-
-
+  logTabsFormHistory(history);
+  toggle.classList.toggle("active");
+} else {
+  chrome.windows.getAll({ populate: true }).then(logTabsForWindows, onError);
+}
 
 // Event handlers
-const clear_button = document.querySelector(".clear-button");
-clear_button.addEventListener("click", function () {
-  chrome.storage.local.clear();
-  document.querySelector(".windows").innerHTML = "";
-  
-});
+clear.addEventListener("click", clearHandler);
+toggle.addEventListener("click", toggleHandler);
+chrome.storage.onChanged.addListener(historyUpdateHandler);
 
+function clearHandler() {
+  if (is_restore_mode) {
+    document.querySelector(".windows").innerHTML = "";
+    writeToStorage("history", []);
+  }
+}
 
-const toggle = document.querySelector(".restore-toggle-button");
-toggle.addEventListener("click", () => {
+function toggleHandler() {
   toggle.classList.toggle("active");
   is_restore_mode = !is_restore_mode;
-  chrome.storage.local.set({mode : is_restore_mode}, function() {
-    if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError.message);
-        return;
+  writeToStorage("mode", is_restore_mode)
+    .then(async () => {
+      if (is_restore_mode) {
+        let history = await readFromStorage("history");
+        if (history == null) {
+          history = [];
+        }
+        logTabsFormHistory(history);
+      } else {
+        chrome.windows
+          .getAll({ populate: true })
+          .then(logTabsForWindows, onError);
+      }
+    })
+    .catch(onError);
+}
+
+let historyUpdateLock = false;
+
+async function historyUpdateHandler() {
+  chrome.storage.onChanged.addListener(async (changes, namespace) => {
+    while (historyUpdateLock) {
+      await new Promise((resolve) => setTimeout(resolve, 50)); // Wait for 10ms before retrying
+    }
+    historyUpdateLock = true;
+    try {
+      if ("history" in changes && is_restore_mode) {
+        let history = await readFromStorage("history");
+        if (history == null) {
+          history = [];
+        }
+        logTabsFormHistory(history);
+      }
+      if ("urls" in changes && !is_restore_mode) {
+        chrome.windows
+          .getAll({ populate: true })
+          .then(logTabsForWindows, onError);
+      }
+    } catch (error) {
+      console.error("Error updating URLs:", error);
+    } finally {
+      historyUpdateLock = false;
     }
   });
-  document.querySelector(".windows").innerHTML = "";
-  if (is_restore_mode) {
-    chrome.storage.local.get({history : []}, function(data) {
-      logTabsFormHistory(data.history);
-    }) 
-  } else {
-    chrome.windows.getAll({populate: true})
-      .then(logTabsForWindows, onError);
-  }
-})
+}
 
 // chrome.tabs.create() -> create a new tab
--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,83 +1,201 @@
-var urls = {};
-var tabId2window = {}
 // Function to update the urls dictionary
-function updateUrls() {
+async function getCurrentUrls() {
+  return new Promise((resolve, reject) => {
+    let urls = {}; // {tab.id : tabinfo}
+    let windowId2TabId = {}; // {windowId : [...tab.id]}
     // Query all windows
-    chrome.windows.getAll({populate: true}, function(windows) {
-        // Clear the existing urls dictionary
-        urls = {};
-        tabId2window = {};
-        
-        // Iterate through each window
-        windows.forEach(function(window) {
-            // Dictionary to store tab IDs mapped to their details
-            var windowTabs = {};
-            
-            // Iterate through each tab in the window
-            window.tabs.forEach(function(tab) {
-                // Add tab details to the windowTabs dictionary
-                windowTabs[tab.id] = {
-                    url: tab.url,
-                    title: tab.title,
-                    favIconUrl: tab.favIconUrl
-                    // Add more properties as needed
-                };
-                tabId2window[tab.id] = window.id;
-            });
-            
-            // Add the windowTabs dictionary to the urls dictionary
-            urls[window.id] = windowTabs;
+    chrome.windows.getAll({ populate: true }, function (windows) {
+      // Clear the existing urls dictionary
+      urls = {};
+      windowId2TabId = {};
+
+      // Iterate through each window
+      windows.forEach(function (window) {
+        // Dictionary to store tab IDs mapped to their details
+        let windowTabs = [];
+
+        // Iterate through each tab in the window
+        window.tabs.forEach(function (tab) {
+          // Add tab details to the windowTabs dictionary
+          urls[tab.id] = {
+            url: tab.url,
+            title: tab.title,
+            favIconUrl: tab.favIconUrl,
+            // Add more properties as needed
+          };
+          windowTabs.push(tab.id);
         });
+
+        // Add the windowTabs dictionary to the urls dictionary
+        windowId2TabId[window.id] = windowTabs;
+      });
+      resolve({ urls, windowId2TabId });
     });
+  });
 }
 
+async function readFromStorage(key) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], (result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result[key] || null);
+      }
+    });
+  });
+}
+
+async function writeToStorage(key, data) {
+  return new Promise((resolve, reject) => {
+    const dataToStore = {};
+    dataToStore[key] = data;
+    chrome.storage.local.set(dataToStore, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+let storageLock = false;
+chrome.runtime.onInstalled.addListener(initializationStorage);
+
 // Listen for when a window is created or removed
-chrome.windows.onCreated.addListener(updateUrls);
+chrome.windows.onCreated.addListener(windowsOnCreatedEventHandler);
 chrome.windows.onRemoved.addListener(storeClosedWindow);
 
 // Listen for when a tab is created, updated, or removed
-chrome.tabs.onCreated.addListener(updateUrls);
-chrome.tabs.onUpdated.addListener(updateUrls);
+chrome.tabs.onCreated.addListener(tabsOnCreatedEventHandler);
+chrome.tabs.onUpdated.addListener(tabsOnUpdatedEventHandler);
 chrome.tabs.onRemoved.addListener(storeClosedTabURL);
 
-// Initial update of the urls dictionary
-updateUrls();
-
-
-function storeClosedTabURL(tabId, removeInfo) {
-    if (removeInfo.isWindowClosing) {
-        return;
-    }
-    let windowId = tabId2window[tabId];
-    let tabInfo = urls[windowId][tabId];
-    chrome.storage.local.get({history: []}, function(data) {
-        var history = data.history || [];
-        history.push([tabInfo]);
-        chrome.storage.local.set({ history: history }, function() {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-                return;
-            }
-        });
-    });
-    updateUrls();
+async function initializationStorage() {
+  let { urls, windowId2TabId } = await getCurrentUrls();
+  await writeToStorage("urls", urls);
+  await writeToStorage("windowId2TabId", windowId2TabId);
 }
 
-function storeClosedWindow (windowId) {
-    let windowInfo = urls[windowId];
-    chrome.storage.local.get({history: []}, function(data) {
-        var history = data.history || [];
-        let windowTabs = [];
-        for (const tab of Object.values(windowInfo)) {
-            windowTabs.push(tab);
-        }
-        history.push(windowTabs);
-        chrome.storage.local.set({ history: history }, function() {
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-                return;
-            }
-        });
-    });
-    updateUrls();
+async function windowsOnCreatedEventHandler() {
+  console.log("windows onCreated event happened");
+  while (storageLock) {
+    await new Promise((resolve) => setTimeout(resolve, 100)); // Wait for 100ms before retrying
+  }
+  storageLock = true;
+  try {
+    let { urls, windowId2TabId } = await getCurrentUrls();
+    await writeToStorage("urls", urls);
+    await writeToStorage("windowId2TabId", windowId2TabId);
+  } catch (error) {
+    console.error("Error updating URLs:", error);
+  } finally {
+    storageLock = false;
+  }
+}
+
+async function tabsOnCreatedEventHandler() {
+  console.log("tabs onCreated event happened");
+  while (storageLock) {
+    await new Promise((resolve) => setTimeout(resolve, 100)); // Wait for 100ms before retrying
+  }
+  storageLock = true;
+  try {
+    let { urls, windowId2TabId } = await getCurrentUrls();
+    await writeToStorage("urls", urls);
+    await writeToStorage("windowId2TabId", windowId2TabId);
+  } catch (error) {
+    console.error("Error updating URLs:", error);
+  } finally {
+    storageLock = false;
+  }
+}
+
+async function tabsOnUpdatedEventHandler() {
+  console.log("tabs onUpdated event happened");
+  while (storageLock) {
+    await new Promise((resolve) => setTimeout(resolve, 100)); // Wait for 100ms before retrying
+  }
+  storageLock = true;
+  try {
+    let { urls, windowId2TabId } = await getCurrentUrls();
+    await writeToStorage("urls", urls);
+    await writeToStorage("windowId2TabId", windowId2TabId);
+  } catch (error) {
+    console.error("Error updating URLs:", error);
+  } finally {
+    storageLock = false;
+  }
+}
+
+async function storeClosedTabURL(tabId, removeInfo) {
+  console.log("storeClosedTabURL");
+  if (removeInfo.isWindowClosing) {
+    return;
+  }
+  const { urls, windowId2TabId } = await getCurrentUrls();
+  while (storageLock) {
+    await new Promise((resolve) => setTimeout(resolve, 10)); // Wait for 10ms before retrying
+  }
+  storageLock = true;
+  const storedUrls = await readFromStorage("urls");
+  // Check if the length of current urls has 1 more tab than stored urls.
+  if (Object.keys(storedUrls).length != Object.keys(urls).length + 1) {
+    throw new Error(
+      "Invalid history update since the number of current urls is not correct.\n" +
+        "current urls: " +
+        Object.keys(urls).length +
+        "\n" +
+        JSON.stringify(urls) +
+        "\n\n" +
+        " stored urls:" +
+        Object.keys(storedUrls).length +
+        "\n" +
+        JSON.stringify(storedUrls)
+    );
+  }
+  // store the closed url into the history
+  let history = await readFromStorage("history");
+  if (history == null) {
+    history = [];
+  }
+  history.push([storedUrls[tabId]]);
+  await writeToStorage("history", history);
+
+  // Update my recording
+  await writeToStorage("urls", urls);
+  await writeToStorage("windowId2TabId", windowId2TabId);
+  storageLock = false;
+}
+
+async function storeClosedWindow(windowId) {
+  console.log("storeClosedWindow");
+  const { urls, windowId2TabId } = await getCurrentUrls();
+  while (storageLock) {
+    await new Promise((resolve) => setTimeout(resolve, 10)); // Wait for 100ms before retrying
+  }
+  storageLock = true;
+  const storedUrls = await readFromStorage("urls");
+  if (Object.keys(urls).length == Object.keys(storedUrls).length) {
+    storageLock = false;
+    return;
+  }
+  const storedWindowId2TabId = await readFromStorage("windowId2TabId");
+  console.log(storedWindowId2TabId);
+  console.log(storedUrls);
+  let windowTabs = [];
+  for (const i in storedWindowId2TabId[windowId]) {
+    windowTabs.push(storedUrls[storedWindowId2TabId[windowId][i]]);
+  }
+  let history = await readFromStorage("history");
+  if (history == null) {
+    history = [];
+  }
+  history.push(windowTabs);
+  await writeToStorage("history", history);
+  // Update my recording
+  await writeToStorage("urls", urls);
+  await writeToStorage("windowId2TabId", windowId2TabId);
+  storageLock = false;
 }
